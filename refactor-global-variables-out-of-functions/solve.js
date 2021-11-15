b242c371a268d1da11e584131591eedcf55c5e50
RomaSushevskij <roma.sushevskij@yandex.ru>
2021-11-15 19:37:18 +0300

    refactor-global-variables-out-of-functions

---
+++ refactor-global-variables-out-of-functions/solve.js
@@ -0,0 +1,30 @@
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add(arr, bookName) {
+
+    let newArr = [...arr]
+    newArr.push(bookName);
+    return newArr
+
+    // Change code above this line
+}
+
+// Change code below this line
+function remove(arr, bookName) {
+    const book_index = bookList.indexOf(bookName);
+    if (book_index >= 0) {
+        let newArr = [...arr]
+        newArr.splice(book_index, 1);
+        return newArr;
+
+        // Change code above this line
+    }
+}
+
+const newBookList = add(bookList, 'A Brief History of Time');
+const newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+const newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
