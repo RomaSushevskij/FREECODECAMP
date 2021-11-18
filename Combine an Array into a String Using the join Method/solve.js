3689d1cb5cbd4cfc7b0d923b747bc9f83136edc2
RomaSushevskij <roma.sushevskij@yandex.ru>
2021-11-18 18:38:24 +0300

    Combine an Array into a String Using the join Method

---
+++ Combine an Array into a String Using the join Method/solve.js
@@ -0,0 +1,10 @@
+function sentensify(str) {
+    // Only change code below this line
+    return str.split(/[-.,]/).join(" ")
+
+    // Only change code above this line
+}
+
+sentensify("May-the-force-be-with-you");
+sentensify("There,has,been,an,awakening")
+sentensify("The.force.is.strong.with.this.one")
