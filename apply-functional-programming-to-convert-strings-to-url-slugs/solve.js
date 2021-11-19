dab0b10919ab6828c429cea0a86e5a01eca951aa
RomaSushevskij <roma.sushevskij@yandex.ru>
2021-11-19 21:43:54 +0300

    apply-functional-programming-to-convert-strings-to-url-slugs

---
+++ apply-functional-programming-to-convert-strings-to-url-slugs/solve.js
@@ -0,0 +1,7 @@
+// Only change code below this line
+function urlSlug(title) {
+    return title.toLowerCase().split(" ").filter(item => item !== "").join("-")
+
+}
+
+// Only change code above this line
