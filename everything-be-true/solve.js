4d2bff84c12d30ae04e1c7b2369e95180b92ba5d
RomaSushevskij <roma.sushevskij@yandex.ru>
2021-12-05 21:22:30 +0300

    everything-be-true

---
+++ everything-be-true/solve.js
@@ -0,0 +1,9 @@
+function truthCheck(collection, pre) {
+    return collection
+        .every(item => item[pre])
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {
+    "user": "Laa-Laa",
+    "sex": "female"
+}, {"user": "Po", "sex": "female"}], "sex");
